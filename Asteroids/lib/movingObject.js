1fe364d2e13e11628eb9278300de869e45d53f84
App Academy Student <student@appacademy.io>
2015-07-27 11:44:45 -0700

    add moving objects class

---
+++ Asteroids/lib/movingObject.js
@@ -0,0 +1,31 @@
+(function (){
+var Asteroids = window.Asteroids = window.Asteroids || {};
+
+function MovingObject(options){
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+}
+// c = new Circle;
+// c.render(canvas)
+MovingObject.prototype.draw = function(ctx){
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(
+    this.pos[0],
+    this.pos[1],
+    this.radius,
+    0,
+    2 * Math.PI,
+    false
+  );
+  ctx.fill();
+};
+
+MovingObject.prototype.move = function() {
+  this.pos[0] += this.vel[0];
+  this.pos[1] += this.vel[1];
+};
+
+})();
